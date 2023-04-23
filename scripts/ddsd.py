8bb4c4af38b01dc994d5db361c3679bcf2e2d407
NeoGraph-K <nnnjjj666666@naver.com>
2023-04-24 02:46:48 +0900

    sd,vae custom update

--- scripts/ddsd.py
+++ scripts/ddsd.py
@@ -1,5 +1,6 @@
 import os
 import math
+import re
 
 import gradio as gr
 from PIL import Image
@@ -9,8 +10,8 @@ from scripts.ddsd_dino import dino_model_list
 from scripts.ddsd_utils import dino_detect_from_prompt, mask_spliter_and_remover, I2I_Generator_Create, get_fonts_list, image_apply_watermark
 
 import modules
-from modules import processing, shared, images, devices, modelloader
-from modules.processing import StableDiffusionProcessingImg2Img, create_infotext, StableDiffusionProcessingTxt2Img
+from modules import processing, shared, images, devices, modelloader, sd_models, sd_vae
+from modules.processing import create_infotext, StableDiffusionProcessingTxt2Img
 from modules.shared import opts, state
 from modules.sd_models import model_hash
 from modules.paths import models_path
@@ -21,6 +22,7 @@ from basicsr.utils.download_util import load_file_from_url
 grounding_models_path = os.path.join(models_path, "grounding")
 sam_models_path = os.path.join(models_path, "sam")
 
+ckpt_model_name_pattern = re.compile('([\\w\\.\\[\\]\\\\]+)\\s*\\[.*\\]')
 
 def list_models(model_path):
         model_list = modelloader.load_models(model_path=model_path, ext_filter=[".pth"])
@@ -81,11 +83,17 @@ class Script(modules.scripts.Script):
         return AlwaysVisible
 
     def ui(self, is_img2img):
+        ckpt_list = list(sd_models.checkpoints_list.keys())
+        ckpt_list.insert(0, 'Original')
+        vae_list = list(sd_vae.vae_dict.keys())
+        vae_list.insert(0, 'Original')
         sample_list = [x.name for x in shared.list_samplers()]
         sample_list = [x for x in sample_list if x not in ['PLMS','UniPC','DDIM']]
         sample_list.insert(0,"Original")
         fonts_list, _ = get_fonts_list()
         ret = []
+        dino_detection_ckpt_list = []
+        dino_detection_vae_list = []
         dino_detection_prompt_list = []
         dino_detection_positive_list = []
         dino_detection_negative_list = []
@@ -125,6 +133,9 @@ class Script(modules.scripts.Script):
                     with gr.Row():
                         upscaler_sample = gr.Dropdown(label='Upscaler Sampling', elem_id='upscaler_sample', choices=sample_list, value=sample_list[0], visible=False, type="value")
                         upscaler_index = gr.Dropdown(label='Upscaler', elem_id='upscaler_index', choices=[x.name for x in shared.sd_upscalers], value=shared.sd_upscalers[-1].name, type="index", visible=False)
+                    with gr.Row():
+                        upscaler_ckpt = gr.Dropdown(label='Upscaler CKPT Model', elem_id=f'upscaler_detect_ckpt', choices=ckpt_list, value=ckpt_list[0], visible=False)
+                        upscaler_vae = gr.Dropdown(label='Upscaler VAE Model', elem_id=f'upscaler_detect_vae', choices=vae_list, value=vae_list[0], visible=False)
                     scalevalue = gr.Slider(minimum=1, maximum=16, step=0.5, elem_id='upscaler_scalevalue', label='Resize', value=2, visible=False)
                     overlap = gr.Slider(minimum=0, maximum=256, step=32, elem_id='upscaler_overlap', label='Tile overlap', value=32, visible=False)
                     with gr.Row():
@@ -145,6 +156,9 @@ class Script(modules.scripts.Script):
                     with gr.Tabs(elem_id = 'dino_detct_arguments', visible=False) as dino_tabs_acc:
                         for index in range(shared.opts.data.get('dino_detect_count', 2)):
                             with gr.Tab(f'DINO {index + 1} Argument', elem_id=f'dino_{index + 1}_argument_tab'):
+                                with gr.Row():
+                                    dino_detection_ckpt = gr.Dropdown(label='Detailer CKPT Model', elem_id=f'detailer_detect_ckpt_{index+1}', choices=ckpt_list, value=ckpt_list[0], visible=True)
+                                    dino_detection_vae = gr.Dropdown(label='Detailer VAE Model', elem_id=f'detailer_detect_vae_{index+1}', choices=vae_list, value=vae_list[0], visible=True)
                                 dino_detection_prompt = gr.Textbox(label=f"Detect {index + 1} Prompt", elem_id=f"detailer_detect_prompt_{index + 1}", show_label=True, lines=2, placeholder="Detect Token Prompt(ex - face:level(0-2):threshold(0-1):dilation(0-128))", visible=True)
                                 with gr.Row():
                                     dino_detection_positive = gr.Textbox(label=f"Positive {index + 1} Prompt", elem_id=f"detailer_detect_positive_{index + 1}", show_label=True, lines=2, placeholder="Detect Mask Inpaint Positive(ex - perfect anatomy)", visible=True)
@@ -154,6 +168,8 @@ class Script(modules.scripts.Script):
                                 dino_detection_steps = gr.Slider(minimum=0, maximum=150, step=1, elem_id=f'dino_detect_{index+1}_steps', label=f'DINO {index + 1} Steps(0 to Origin)', value=0, visible=True)
                                 dino_detection_spliter_disable = gr.Checkbox(label=f'Disable DINO {index + 1} Detect Split Mask', value=True, visible=True)
                                 dino_detection_spliter_remove_area = gr.Slider(minimum=0, maximum=800, step=8, elem_id=f'dino_detect_{index+1}_remove_area', label=f'Remove {index + 1} Area', value=16, visible=True)
+                                dino_detection_ckpt_list.append(dino_detection_ckpt)
+                                dino_detection_vae_list.append(dino_detection_vae)
                                 dino_detection_prompt_list.append(dino_detection_prompt)
                                 dino_detection_positive_list.append(dino_detection_positive)
                                 dino_detection_negative_list.append(dino_detection_negative)
@@ -223,6 +239,8 @@ class Script(modules.scripts.Script):
                 ddetailer_before_upscaler:gr_show(not disable),
                 upscaler_sample:gr_show(not disable),
                 upscaler_index:gr_show(not disable),
+                upscaler_ckpt:gr_show(not disable),
+                upscaler_vae:gr_show(not disable),
                 scalevalue:gr_show(not disable),
                 overlap:gr_show(not disable),
                 rewidth:gr_show(not disable),
@@ -230,7 +248,7 @@ class Script(modules.scripts.Script):
                 denoising_strength:gr_show(not disable),
             },
             inputs= [disable_upscaler],
-            outputs =[ddetailer_before_upscaler, upscaler_sample, upscaler_index, scalevalue, overlap, rewidth, reheight, denoising_strength]
+            outputs =[ddetailer_before_upscaler, upscaler_sample, upscaler_index, upscaler_ckpt, upscaler_vae, scalevalue, overlap, rewidth, reheight, denoising_strength]
         )
         
         disable_mask_paint_mode.change(
@@ -269,9 +287,11 @@ class Script(modules.scripts.Script):
         
         ret += [enable_script_names]
         ret += [disable_watermark]
-        ret += [disable_upscaler, ddetailer_before_upscaler, scalevalue, upscaler_sample, overlap, upscaler_index, rewidth, reheight, denoising_strength]
+        ret += [disable_upscaler, ddetailer_before_upscaler, scalevalue, upscaler_sample, overlap, upscaler_index, rewidth, reheight, denoising_strength, upscaler_ckpt, upscaler_vae]
         ret += [disable_detailer, disable_mask_paint_mode, inpaint_mask_mode, detailer_sample, detailer_sam_model, detailer_dino_model, dino_full_res_inpaint, dino_inpaint_padding, detailer_mask_blur]
-        ret += dino_detection_prompt_list + \
+        ret += dino_detection_ckpt_list + \
+                dino_detection_vae_list + \
+                dino_detection_prompt_list + \
                 dino_detection_positive_list + \
                 dino_detection_negative_list + \
                 dino_detection_denoise_list + \
@@ -297,6 +317,8 @@ class Script(modules.scripts.Script):
                              disable_mask_paint_mode, inpaint_mask_mode, detailer_sample, detailer_sam_model, detailer_dino_model,
                              dino_full_res_inpaint, dino_inpaint_padding, detailer_mask_blur,
                              dino_detect_count,
+                             dino_detection_ckpt_list,
+                             dino_detection_vae_list,
                              dino_detection_prompt_list,
                              dino_detection_positive_list,
                              dino_detection_negative_list,
@@ -306,6 +328,8 @@ class Script(modules.scripts.Script):
                              dino_detection_spliter_disable_list,
                              dino_detection_spliter_remove_area_list):
         for detect_index in range(dino_detect_count):
+            self.change_ckpt_model(dino_detection_ckpt_list[detect_index] if dino_detection_ckpt_list[detect_index] != 'Original' else self.ckptname)
+            self.change_vae_model(dino_detection_vae_list[detect_index] if dino_detection_vae_list[detect_index] != 'Original' else self.vae)
             if len(dino_detection_prompt_list[detect_index]) < 1: continue
             pi = I2I_Generator_Create(
                 p, ('Euler' if p.sampler_name in ['PLMS', 'UniPC', 'DDIM'] else p.sampler_name) if detailer_sample == 'Original' else detailer_sample,
@@ -379,8 +403,10 @@ class Script(modules.scripts.Script):
         return init_image
     
     def upscale(self, p, init_image, 
-                scalevalue, upscaler_sample, overlap, rewidth, reheight, denoising_strength, 
+                scalevalue, upscaler_sample, overlap, rewidth, reheight, denoising_strength, upscaler_ckpt, upscaler_vae,
                 detailer_mask_blur, dino_full_res_inpaint, dino_inpaint_padding):
+        self.change_ckpt_model(upscaler_ckpt if upscaler_ckpt != 'Original' else self.ckptname)
+        self.change_vae_model(upscaler_vae if upscaler_vae != 'Original' else self.vae)
         pi = I2I_Generator_Create(
                 p, ('Euler' if p.sampler_name in ['PLMS', 'UniPC', 'DDIM'] else p.sampler_name) if upscaler_sample == 'Original' else upscaler_sample,
                 detailer_mask_blur, dino_full_res_inpaint, dino_inpaint_padding, init_image,
@@ -455,15 +481,33 @@ class Script(modules.scripts.Script):
                                                 self.watermark_padding_list[water_index],
                                                 self.watermark_alpha_list[water_index])
         return init_image
+
+    def change_vae_model(self, name:str):
+        if name in ['auto', 'automatic']: modules.sd_vae.reload_vae_weights(shared.sd_model, vae_file=modules.sd_vae.unspecified)
+        elif name == 'none': modules.sd_vae.reload_vae_weights(shared.sd_model, vae_file=None)
+        else: modules.sd_vae.reload_vae_weights(shared.sd_model, vae_file=modules.sd_vae.vae_dict[name])
+    
+    def change_ckpt_model(self, name:str):
+        info = modules.sd_models.get_closet_checkpoint_match(name)
+        if info is None:
+            raise RuntimeError(f"Unknown checkpoint: {name}")
+        modules.sd_models.reload_model_weights(shared.sd_model, info)
+    
+    def postprocess(self, p, res, *args, **kargs):
+        if getattr(p, 'sub_processing', False): return
+        self.change_ckpt_model(self.ckptname)
+        self.change_vae_model(self.vae)
     
     def process(self, p,
             enable_script_names,
             disable_watermark,
-            disable_upscaler, ddetailer_before_upscaler, scalevalue, upscaler_sample, overlap, upscaler_index, rewidth, reheight, denoising_strength,
+            disable_upscaler, ddetailer_before_upscaler, scalevalue, upscaler_sample, overlap, upscaler_index, rewidth, reheight, denoising_strength, upscaler_ckpt, upscaler_vae,
             disable_detailer, disable_mask_paint_mode, inpaint_mask_mode, detailer_sample, detailer_sam_model, detailer_dino_model,
             dino_full_res_inpaint, dino_inpaint_padding, detailer_mask_blur,
             *args):
         if getattr(p, 'sub_processing', False): return
+        self.ckptname = ckpt_model_name_pattern.search(shared.opts.data['sd_model_checkpoint']).group(1)
+        self.vae = shared.opts.data['sd_vae']
         self.restore_script(p)
         self.enable_script_names = enable_script_names
         self.disable_watermark = disable_watermark
@@ -476,6 +520,8 @@ class Script(modules.scripts.Script):
         self.rewidth = rewidth
         self.reheight = reheight
         self.denoising_strength = denoising_strength
+        self.upscaler_ckpt = upscaler_ckpt
+        self.upscaler_vae = upscaler_vae
         self.disable_detailer = disable_detailer
         self.disable_mask_paint_mode = disable_mask_paint_mode
         self.inpaint_mask_mode = inpaint_mask_mode
@@ -487,26 +533,28 @@ class Script(modules.scripts.Script):
         self.detailer_mask_blur = detailer_mask_blur
         args_list = [*args]
         self.dino_detect_count = shared.opts.data.get('dino_detect_count', 2)
-        self.dino_detection_prompt_list = args_list[self.dino_detect_count * 0:self.dino_detect_count * 1]
-        self.dino_detection_positive_list = args_list[self.dino_detect_count * 1:self.dino_detect_count * 2]
-        self.dino_detection_negative_list = args_list[self.dino_detect_count * 2:self.dino_detect_count * 3]
-        self.dino_detection_denoise_list = args_list[self.dino_detect_count * 3:self.dino_detect_count * 4]
-        self.dino_detection_cfg_list = args_list[self.dino_detect_count * 4:self.dino_detect_count * 5]
-        self.dino_detection_steps_list = args_list[self.dino_detect_count * 5:self.dino_detect_count * 6]
-        self.dino_detection_spliter_disable_list = args_list[self.dino_detect_count * 6:self.dino_detect_count * 7]
-        self.dino_detection_spliter_remove_area_list = args_list[self.dino_detect_count * 7:self.dino_detect_count * 8]
+        self.dino_detection_ckpt_list = args_list[self.dino_detect_count * 0:self.dino_detect_count * 1]
+        self.dino_detection_vae_list = args_list[self.dino_detect_count * 1:self.dino_detect_count * 2]
+        self.dino_detection_prompt_list = args_list[self.dino_detect_count * 2:self.dino_detect_count * 3]
+        self.dino_detection_positive_list = args_list[self.dino_detect_count * 3:self.dino_detect_count * 4] 
+        self.dino_detection_negative_list = args_list[self.dino_detect_count * 4:self.dino_detect_count * 5]
+        self.dino_detection_denoise_list = args_list[self.dino_detect_count * 5:self.dino_detect_count * 6]
+        self.dino_detection_cfg_list = args_list[self.dino_detect_count * 6:self.dino_detect_count * 7]
+        self.dino_detection_steps_list = args_list[self.dino_detect_count * 7:self.dino_detect_count * 8]
+        self.dino_detection_spliter_disable_list = args_list[self.dino_detect_count * 8:self.dino_detect_count * 9]
+        self.dino_detection_spliter_remove_area_list = args_list[self.dino_detect_count * 9:self.dino_detect_count * 10]
         self.watermark_count = shared.opts.data.get('watermark_count', 1)
-        self.watermark_type_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 0:self.dino_detect_count * 8 + self.watermark_count * 1]
-        self.watermark_position_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 1:self.dino_detect_count * 8 + self.watermark_count * 2]
-        self.watermark_image_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 2:self.dino_detect_count * 8 + self.watermark_count * 3]
-        self.watermark_image_size_width_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 3:self.dino_detect_count * 8 + self.watermark_count * 4]
-        self.watermark_image_size_height_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 4:self.dino_detect_count * 8 + self.watermark_count * 5]
-        self.watermark_text_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 5:self.dino_detect_count * 8 + self.watermark_count * 6]
-        self.watermark_text_color_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 6:self.dino_detect_count * 8 + self.watermark_count * 7]
-        self.watermark_text_font_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 7:self.dino_detect_count * 8 + self.watermark_count * 8]
-        self.watermark_text_size_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 8:self.dino_detect_count * 8 + self.watermark_count * 9]
-        self.watermark_padding_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 9:self.dino_detect_count * 8 + self.watermark_count * 10]
-        self.watermark_alpha_list = args_list[self.dino_detect_count * 8 + self.watermark_count * 10:self.dino_detect_count * 8 + self.watermark_count * 11]
+        self.watermark_type_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 0:self.dino_detect_count * 10 + self.watermark_count * 1]
+        self.watermark_position_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 1:self.dino_detect_count * 10 + self.watermark_count * 2]
+        self.watermark_image_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 2:self.dino_detect_count * 10 + self.watermark_count * 3]
+        self.watermark_image_size_width_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 3:self.dino_detect_count * 10 + self.watermark_count * 4]
+        self.watermark_image_size_height_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 4:self.dino_detect_count * 10 + self.watermark_count * 5]
+        self.watermark_text_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 5:self.dino_detect_count * 10 + self.watermark_count * 6]
+        self.watermark_text_color_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 6:self.dino_detect_count * 10 + self.watermark_count * 7]
+        self.watermark_text_font_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 7:self.dino_detect_count * 10 + self.watermark_count * 8]
+        self.watermark_text_size_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 8:self.dino_detect_count * 10 + self.watermark_count * 9]
+        self.watermark_padding_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 9:self.dino_detect_count * 10 + self.watermark_count * 10]
+        self.watermark_alpha_list = args_list[self.dino_detect_count * 10 + self.watermark_count * 10:self.dino_detect_count * 10 + self.watermark_count * 11]
         self.script_names_list = [x.strip()+'.py' for x in enable_script_names.split(';') if len(x) > 1]
         self.script_names_list += [os.path.basename(__file__)]
         self.i2i_scripts = [x for x in self.original_scripts if os.path.basename(x.filename) in self.script_names_list].copy()
@@ -554,6 +602,8 @@ class Script(modules.scripts.Script):
                                                      self.disable_mask_paint_mode, self.inpaint_mask_mode, self.detailer_sample, self.detailer_sam_model, self.detailer_dino_model, 
                                                      self.dino_full_res_inpaint, self.dino_inpaint_padding, self.detailer_mask_blur,
                                                      self.dino_detect_count,
+                                                     self.dino_detection_ckpt_list,
+                                                     self.dino_detection_vae_list,
                                                      self.dino_detection_prompt_list,
                                                      self.dino_detection_positive_list,
                                                      self.dino_detection_negative_list,
